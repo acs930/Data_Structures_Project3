42d3329885c40af7fc006f4af47dfd7a0a2bba99
acs930 <acs930@gmail.com>
2013-04-22 13:03:27 -0300

    Create StackInterface.java

---
+++ StackInterface.java
@@ -0,0 +1,16 @@
+//Adam Smith
+
+public interface StackInterface<E> {
+
+ public int size();
+
+
+ public boolean isEmpty();
+
+ public E top();
+
+
+  public void push(E element); 
+
+ public E pop();
+}   
